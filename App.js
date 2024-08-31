bef3a6e7dffac20cced7e0efc40b6919f26a3a50
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-08-31 18:55:05 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [lowerLimit, setLowerLimit] = useState(null);
+  const [upperLimit, setUpperLimit] = useState(null);
+
+  const calculateHeartRateLimits = () => {
+    const ageNumber = parseInt(age);
+    if (!isNaN(ageNumber)) {
+      const lower = (220 - ageNumber) * 0.65;
+      const upper = (220 - ageNumber) * 0.85;
+      setLowerLimit(lower.toFixed(0));
+      setUpperLimit(upper.toFixed(0));
+    } else {
+      setLowerLimit(null);
+      setUpperLimit(null);
+      alert('Please enter a valid age');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput
+        style={styles.field} 
+        placeholder="0"
+        value={age}
+        onChangeText={(text) => setAge(text)}
+        keyboardType="decimal-pad"
+      />
+      <Text>Limits</Text>
+      <Text style={styles.field} >{lowerLimit}-{upperLimit}</Text>
+      <Button title="Calculate" onPress={calculateHeartRateLimits} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +43,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingTop: 30,
+    margin: 8,
   },
+  field:{
+    marginTop: 8,
+    marginBottom: 8,
+  }
 });
